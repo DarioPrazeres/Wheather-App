885428105014b91c50dc26facf35f105c1a0ab0f
Dario Prazeres <darioedgarprazeres@gmail.com>
2021-12-27 20:46:58 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'docs'),
   },
+  module: {
+    rules: [
+    {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+    },
+    {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    },
+    {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+    },
+],
+},
 };
